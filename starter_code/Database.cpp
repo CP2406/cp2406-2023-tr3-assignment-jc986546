9fd46e011c7f459608ad893ad0d46e7011d92e69
jc986546 <151517410+jc986546@users.noreply.github.com>
2023-11-25 23:04:24 +1000

    Update Database.cpp
    Generate new database, A function that generates random addresses for employees.

--- starter_code/Database.cpp
+++ starter_code/Database.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 #include <stdexcept>
+#include <random>
+#include <fstream>
+#include <chrono>
+#include <unistd.h>
+#include <sstream>
+#include <random>
 #include "Database.h"
 
 using namespace std;
@@ -62,3 +68,47 @@ namespace Records {
 	}
 
 }
+   /**
+     * @brief Generate a new database with 8000 employees
+    */
+    void Database::generateNewDatabase()
+    {
+        cout << "Generating new database..." << endl;
+        // Generate 20 first names, middle names, and last names.
+        string firstNames[20] = {"Jacob", "Michael", "Joshua", "Matthew", "Ethan", "Andrew", "Daniel", "Anthony", "Christopher", "Joseph", "William", "Alexander", "Ryan", "David", "Nicholas", "Tyler", "James", "John", "Jonathan", "Nathan"};
+        string middleNames[20] = {"Aaron", "Adam", "Alan", "Albert", "Alexander", "Andrew", "Anthony", "Arthur", "Benjamin", "Bruce", "Carl", "Charles", "Christopher", "Daniel", "David", "Donald", "Douglas", "Edward", "Eric", "Ethan"};
+        string lastNames[20] = {"Smith", "Johnson", "Williams", "Jones", "Brown", "Davis", "Miller", "Wilson", "Moore", "Taylor", "Anderson", "Thomas", "Jackson", "White", "Harris", "Martin", "Thompson", "Garcia", "Martinez", "Robinson"};
+        const std::string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        random_device seed;
+        ranlux48 engine(seed());
+        // Generate a random number between 8000 and 10000000.
+        uniform_int_distribution<> distrib1(8000, 10000000);
+        uniform_int_distribution<> distrib(0, characters.length() - 1);
+
+        // Generate 8000 employees.
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 20; ++j) {
+                for (int k = 0; k < 20; ++k) {
+                    Employee& employee = addEmployee(firstNames[i], lastNames[j]);
+                    employee.setMiddleName(middleNames[k]);
+                    auto now = std::chrono::steady_clock::now();
+                    auto duration = now.time_since_epoch();
+                    auto timestamp = std::chrono::duration_cast<std::chrono::microseconds>(duration).count();
+
+                    std::string randomString;
+                    randomString.reserve(20);
+                    int random_number = distrib1(engine);
+                    // Generate a random string of 20 characters.
+                    for (int r = 0; r < 20; ++r) {
+                        randomString += characters[distrib(engine)];
+                    }
+                    // Generate a unique address for each employee.
+                    std::string unique_address = "Addr" + std::to_string(timestamp) + std::to_string(random_number) + randomString;
+                    employee.setAddress(unique_address);
+                }
+            }
+        }
+
+        cout << "Done!" << endl;
+    }
